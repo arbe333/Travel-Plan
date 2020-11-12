8a4fb5f0a118f2bc0d5e20eef0d411b7d333c908
Arbe <arbe0102@gmail.com>
2020-11-12 21:12:50 +0800

    Initial Commit

---
+++ Travel Plan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel Plan
+//
+//  Created by arbe on 2020/11/12.
+//  Copyright © 2020 arbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
